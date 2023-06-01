8983dfe7d3ed7c342473d1de94e6acda20ca0a61
osmar <osmarernesto17@gmail.com>
2023-06-01 09:01:38 -0600

    feat: divisa

--- divisa.py
+++ divisa.py
@@ -1,2 +1,16 @@
 
-divisa = {"Euro":""}
+divisa = {"Euro":"€", "Dollar":"$", "Yen":"¥"}
+
+entrada = input("Que divisa? ");
+r = None
+for div in divisa:
+    if div == entrada:
+        r = divisa[div]
+        break
+
+if r != None:
+    print(r)
+else:
+    print("No se encontro la divisa")
+
+   
